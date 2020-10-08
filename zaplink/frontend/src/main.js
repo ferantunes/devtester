e05440658e28743f127e943609c7bd293b505d17
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-07 22:17:06 -0300

    Integrando o Bulma

--- zaplink/frontend/src/main.js
+++ zaplink/frontend/src/main.js
@@ -2,6 +2,12 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 
+//É uma versão do BULMA para vue js
+import Buefy from 'buefy'
+import 'buefy/dist/buefy.css'
+
+Vue.use(Buefy)
+
 Vue.config.productionTip = false
 
 new Vue({
