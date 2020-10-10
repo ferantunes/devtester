faa9592f3b06199f68bf533d54bc1110ee13ab38
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-09 21:46:35 -0300

    Navbar Component

--- zaplink/frontend/src/views/Dashboard.vue
+++ zaplink/frontend/src/views/Dashboard.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="dashboard">
     <div class="container">
-      <h1>Seu gerenciador digital de contatos</h1>
+      <h4 class="title is-4">Seu gerenciador digital de contatos</h4>
 
       <div class="columns">
         <div class="column is-4" v-for="contact in contactList" :key="contact.id">
@@ -10,10 +10,7 @@
               <div class="media">
                 <div class="media-left">
                   <figure class="image is-48x48">
-                    <img
-                      src="https://bulma.io/images/placeholders/96x96.png"
-                      alt="Placeholder image"
-                    />
+                    <img src="../assets/whatsapp.svg" alt="Logo WhatsApp"/>
                   </figure>
                 </div>
                 <div class="media-content">
