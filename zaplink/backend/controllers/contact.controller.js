48ca360d0ff9284c4f57e1328fc7c9a3a3eef564
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-10 17:22:50 -0300

    Criando um método POST

--- zaplink/backend/controllers/contact.controller.js
+++ zaplink/backend/controllers/contact.controller.js
@@ -2,6 +2,20 @@ const contactModel = require('../models/contact.model');
 const { concat } = require('../routes/contact.routes');
 
 module.exports = {
+    async create(request, h) {
+
+        console.log(request.payload);
+
+        const contact = new contactModel({
+            name: request.payload.name,
+            number: request.payload.number,
+            description: request.payload.description
+        });
+
+        contact.save();
+    },
+
+
     async list(request, h) {
         const contacts = await contactModel.find().exec();
         return contacts;
--- zaplink/backend/routes/contact.routes.js
+++ zaplink/backend/routes/contact.routes.js
@@ -5,5 +5,10 @@ module.exports = [
         method: 'GET',
         path: '/contacts',
         handler: contactController.list
+    },
+    {
+        method: 'POST',
+        path: '/contacts',
+        handler: contactController.create
     }
 ]
