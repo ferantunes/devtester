4813d2a71850151db210a213a5ded6ff66d600b4
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-09 22:17:07 -0300

    Iniciando o Backend

---
+++ zaplink/backend/routes/contact.routes.js
@@ -0,0 +1,9 @@
+const contactController = require('../controllers/contact.controller');
+
+module.exports = [
+    {
+        method: 'GET',
+        path: '/contacts',
+        handler: contactController.list
+    }
+]
