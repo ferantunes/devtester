3e3580bf85fe39530cce8c67ada48c810741a880
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-10 17:10:54 -0300

    Início do módulo Fase 2 - Cadastrar Contatos - Adicionando um Modal

--- zaplink/frontend/src/views/Dashboard.vue
+++ zaplink/frontend/src/views/Dashboard.vue
@@ -3,6 +3,8 @@
     <div class="container">
       <h4 class="title is-4">Seu gerenciador digital de contatos</h4>
 
+      <button class="button is-success is-medium" @click="showContactAddModal = true">+</button>
+
       <div class="columns is-multiline">
         <div class="column is-4" v-for="contact in contactList" :key="contact.id">
           <div class="card">
@@ -10,17 +12,17 @@
               <div class="media">
                 <div class="media-left">
                   <figure class="image is-48x48">
-                    <img src="../assets/whatsapp.svg" alt="Logo WhatsApp"/>
+                    <img src="../assets/whatsapp.svg" alt="Logo WhatsApp" />
                   </figure>
                 </div>
                 <div class="media-content">
-                  <p class="title is-4">{{contact.name}}</p>
-                  <p class="subtitle is-6">{{contact.number}}</p>
+                  <p class="title is-4">{{ contact.name }}</p>
+                  <p class="subtitle is-6">{{ contact.number }}</p>
                 </div>
               </div>
 
               <div class="content">
-                {{contact.description}}
+                {{ contact.description }}
               </div>
             </div>
             <footer class="card-footer">
@@ -30,6 +32,31 @@
           </div>
         </div>
       </div>
+
+      <b-modal v-model="showContactAddModal" has-modal-card trap-focus :destroy-on-hide="false" aria-role="dialog" aria-modal>
+        <form action="">
+                <div class="modal-card" style="width: 450px">
+                    <header class="modal-card-head">
+                        <p class="modal-card-title">Novo Contato</p>
+                        <button type="button" class="delete" @click="showContactAddModal = false"/>
+                    </header>
+                    <section class="modal-card-body">
+                        <div class="field">
+                          <input class="input is-primary" placeholder="Nome completo">
+                        </div>
+                        <div class="field">
+                          <input class="input is-primary" placeholder="WhatsApp">
+                        </div>
+                        <div class="field">
+                          <textarea class="textarea is-primary" placeholder="Assunto"></textarea>
+                        </div>
+                    </section>
+                    <footer class="modal-card-foot">
+                        <button class="button is-success">Cadastrar</button>
+                    </footer>
+                </div>
+            </form>
+      </b-modal>
     </div>
   </div>
 </template>
@@ -42,17 +69,18 @@ export default {
   data() {
     return {
       contactList: [],
+      showContactAddModal: false,
     };
   },
   methods: {
     list() {
-      window.axios.get('/contacts').then(async (res) => {
+      window.axios.get("/contacts").then(async (res) => {
         this.contactList = await res.data;
-      })
-    }
+      });
+    },
   },
   mounted() {
     this.list();
-  }
+  },
 };
 </script>
