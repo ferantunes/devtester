9d544217ac2164d349a91cf1355fdf88a795c930
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-11 12:14:11 -0300

    Testando a Busca de Contatos

---
+++ zaplink/frontend/tests/e2e/specs/contact_search_spec.js
@@ -0,0 +1,21 @@
+describe('Busca', () => {
+
+    const contact = {
+        name: 'Mike Portnoy',
+        number: '21987654321',
+        description: 'Aulas de Bateria'
+    }
+    //Apostofro, é usado quando quero fazer uma interpolação com alguma informação
+    describe(`Quando busco pelo contato ${contact.name}`, () =>{
+        before(() => {
+            cy.dash();
+            cy.searchContact(contact.number);
+        })
+
+        it('Devo ver somente esse contato no dashboard', () => {
+            cy.contactItem().should('have.length', 1);
+            cy.contactItem().contains(contact.name);
+            cy.contactItem().contains(contact.description);
+        })
+    })
+})
