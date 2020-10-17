6b62c3b3a4b9ad5482122bf81c7a5d2461a9f405
Fernanda Antunes <fernanda.modeneze@gmail.com>
2020-10-17 14:58:09 -0300

    Interceptando requisições

--- zaplink/frontend/src/main.js
+++ zaplink/frontend/src/main.js
@@ -11,6 +11,16 @@ window.axios = require('axios');
 //configuração da url padrão da API que vai ser consumido pelo VUE.js
 window.axios.defaults.baseURL = 'http://localhost:3000';
 
+//Intercepta todas as requisições e passamos a chave do token automaticamente o token
+window.axios.interceptors.request.use((config) => {
+  const userToken = localStorage.getItem('user_token');
+
+  if(userToken)
+    config.headers.authorization = userToken;
+
+  return config;
+}, (erro) => Promise.reject(erro));
+
 Vue.use(Buefy)
 
 Vue.config.productionTip = false
